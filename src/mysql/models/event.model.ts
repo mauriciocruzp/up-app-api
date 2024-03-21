7e52da3884b41c777c6c2f7026e451ed20b6e0a6
FernandaFloresVera <14mayfeflo@gmail.com>
2024-03-21 08:54:13 -0600

    Add event model

---
+++ src/mysql/models/event.model.ts
@@ -0,0 +1,54 @@
+import { Column, DataType, Model, Table } from "sequelize-typescript";
+
+export interface EventAttributes {
+    id: string;
+    title: string;
+    description: string;
+    date: Date;
+    location: string;
+    image: string;
+}
+
+@Table({
+    tableName: 'events',
+    modelName: 'Event',
+    timestamps: false,
+})
+
+export default class EventModel extends Model implements EventAttributes {
+    @Column({
+        primaryKey: true,
+        type: DataType.STRING,
+    })
+    declare id: string;
+
+    @Column({
+        allowNull: false,
+        type: DataType.STRING,
+    })
+    declare title: string;
+
+    @Column({
+        allowNull: false,
+        type: DataType.STRING,
+    })
+    declare description: string;
+
+    @Column({
+        allowNull: false,
+        type: DataType.DATE,
+    })
+    declare date: Date;
+
+    @Column({
+        allowNull: false,
+        type: DataType.STRING,
+    })
+    declare location: string;
+
+    @Column({
+        allowNull: false,
+        type: DataType.STRING,
+    })
+    declare image: string;
+}
